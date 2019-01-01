38600853d1797f0558ee53c0a4a9345531c019d5
Tiafeno <tiafenofnel@gmail.com>
2019-01-01 10:09:07 +0100

    Fix error handler

--- src/app/index/login/login.component.ts
+++ src/app/index/login/login.component.ts
@@ -7,50 +7,57 @@ import { Router } from '@angular/router';
 declare var $: any;
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
+   selector: 'app-login',
+   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
-  public error: boolean = false;
-  public submitted: boolean = false;
-  public form: FormGroup;
-  public loading: boolean = false;
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {
-    this.form = new FormGroup({
-      email: new FormControl('', Validators.required),
-      pwd: new FormControl('', Validators.required),
-    });
-  }
+   public error: boolean = false;
+   public submitted: boolean = false;
+   public form: FormGroup;
+   public loading: boolean = false;
+   constructor(
+      private authService: AuthService,
+      private router: Router
+   ) {
+      this.form = new FormGroup({
+         email: new FormControl('', Validators.required),
+         pwd: new FormControl('', Validators.required),
+      });
+   }
 
-  ngOnInit() {
-    this.authService.logout();
-    $('body').addClass('empty-layout');
-  }
+   ngOnInit() {
+      this.authService.logout();
+      $('body').addClass('empty-layout');
+   }
 
-  get f() { return this.form.controls; }
+   get f() { return this.form.controls; }
 
-  onSubmit() {
-    this.error = false;
-    this.submitted = true;
-    // stop here if form is invalid
-    if (this.form.invalid) {
-      return;
-    }
-    this.loading = true;
-    this.authService.login(this.form.value.email, this.form.value.pwd)
-      .pipe(first())
-      .subscribe(
-        data => {
-          if (data)
-            this.router.navigate(['/home']);
-        },
-        error => {
-          this.error = true;
-          this.loading = false;
-        });
-  }
+   onSubmit() {
+      this.error = false;
+      this.submitted = true;
+      // stop here if form is invalid
+      if (this.form.invalid) {
+         return;
+      }
+      this.loading = true;
+      this.authService.login(this.form.value.email, this.form.value.pwd)
+         .pipe(first())
+         .subscribe(
+            data => {
+               if (data) {
+                  this.router.navigate(['/home']);
+               } else {
+                  this.errorHandler();
+               }
+            },
+            error => {
+               this.errorHandler();
+            });
+   }
+
+   public errorHandler() {
+      this.error = true;
+      this.loading = false;
+   }
 
 }
